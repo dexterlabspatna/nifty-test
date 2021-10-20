1d37f0986e104d353c70c25fad600c409658dfcf
dexterlabspatna <88551522+dexterlabspatna@users.noreply.github.com>
2021-10-20 15:08:30 +0530

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,7 +1,9 @@
-print("ok")
-
-"""from py5paisa import FivePaisaClient
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+from py5paisa import FivePaisaClient
 from py5paisa.order import Order, OrderType, Exchange
+import json
+
 cred={
     "APP_NAME":"5P56685593",
     "APP_SOURCE":"7684",
@@ -14,7 +16,59 @@ cred={
 client = FivePaisaClient(email="abhis.s2009@gmail.com", passwd="abhi2010", dob="19891201",cred=cred)
 client.login()
 
+def order(OrderType, Exchange, ExchangeSegment, ScripCode, Quantity, Price, IsIntraday):
+    try:
+        print("sending order")
+        order = Order(order_type=OrderType,exchange=Exchange,exchange_segment=ExchangeSegment, scrip_code = ScripCode, quantity=Quantity, price=Price,is_intraday=IsIntraday)
+        place_order = client.place_order(order)
+        print(place_order)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
 
-test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code = 37848, quantity=50, price=236.5,is_intraday=True)
-client.place_order(test_order)
-"""
+    return order
+    
+    
+@app.route('/')
+def hello_world():
+    return 'Hello, Nagesh'
+
+@app.route('/login')
+def whatever():
+  print("hello")
+  login_response = client.login()
+  print(login_response)
+  return{
+    "message": "Invalid passphrase"
+}
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print(request.data)
+    data = json.loads(request.data)
+
+    OrderType = data['order_type']
+    Exchange = data['exchange']
+    ExchangeSegment = data['exchange_segment']
+    ScripCode = data['scrip_code']
+    Quantity = data['quantity']
+    Price = data['price']
+    IsIntraday = data['is_intraday']
+    print(OrderType)
+    """
+    side = data['strategy']['order_action'].upper()
+    tick = data['ticker']
+    quantity = data['strategy']['order_contracts']
+    formatQuantity = format(quantity, '.5')
+    """
+    order_response = order(OrderType, Exchange, ExchangeSegment, ScripCode, Quantity, Price, IsIntraday)
+
+    print(order_response)
+
+    return{
+        "code": "error",
+        "message": "order"
+    }
+
+#test_order = Order(order_type='B',exchange='N',exchange_segment='D', scrip_code = 37848, quantity=50, price=236.5,is_intraday=True)
+#client.place_order(test_order)
